2d0f8765436e1f72ecec18bea51ef4fbc6420956
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-10 11:27:45 +0530

    [Ganesh] Add:Daily wage is added

--- EmployeeWage/ComputeEmployee.cs
+++ EmployeeWage/ComputeEmployee.cs
@@ -9,7 +9,7 @@ namespace EmployeeWage
 {
     public class ComputeEmployee
     {
-        const int IS_PRESENT = 1;
+        const int IS_PRESENT = 1, WAGE_PER_HR = 20, NO_OF_HRS = 8;
         public void attandance()
         {
             Random random = new Random();
@@ -18,6 +18,15 @@ namespace EmployeeWage
                 Console.WriteLine("Employee is present");
             else 
                 Console.WriteLine("Employee is absent");
+        }
+        public void wagePerDay()
+        {
+            int wage = 0;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == IS_PRESENT)
+                wage = WAGE_PER_HR * NO_OF_HRS;
+            Console.WriteLine("Daily wage of an employee is: "+wage);
         }
     }
 }
