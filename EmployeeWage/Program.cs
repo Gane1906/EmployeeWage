d50c9632d22ec9b83f401a06a3e73d30d3f565a3
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-10 11:50:40 +0530

    [Ganesh] Add:Monthly wage is added

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -8,7 +8,7 @@ namespace EmployeeWage
             Console.WriteLine("Welcome to Employee problem");
             ComputeEmployee computeEmployee = new ComputeEmployee();
             computeEmployee.attandance();
-            computeEmployee.wagePerDay();
+            computeEmployee.calculateWage();
         }
     } 
 }
