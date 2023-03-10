7caaa6c4cf784dd8c95bced9a32a4f4336ea8828
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-10 11:12:14 +0530

    [Ganesh] Add: Employee wage file is added

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmployeeWage
+{
+    public class Employee
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee problem");
+        }
+    } 
+}
